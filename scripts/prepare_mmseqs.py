2d3eb0d8915f364e662a47b399fa6f46c521dc4b
Jannik Gut <jannik.gut@unibe.ch>
2025-12-16 23:01:40 +0100

    Add files via upload

---
+++ scripts/prepare_mmseqs.py
@@ -0,0 +1,140 @@
+from unifold.msa.pipeline import make_msa_features, make_sequence_features
+from unifold.msa import parsers
+from unifold.colab.data import load_fasta, validate_input
+import pickle
+import os
+import gzip
+import re
+import hashlib
+from pathlib import Path
+from unifold.msa.utils import divide_multi_chains
+from unifold.data.utils import compress_features
+from unifold.data.protein import PDB_CHAIN_IDS
+from unifold.colab.mmseqs import get_msa_and_templates
+import shutil
+import argparse
+
+MIN_SINGLE_SEQUENCE_LENGTH = 6
+MAX_SINGLE_SEQUENCE_LENGTH = 3000
+MAX_MULTIMER_LENGTH = 3000
+
+def make_mmseqs(fasta_path, output_dir_base, use_templates):
+    jobname = "curr_job"
+    input_sequences, descriptions = load_fasta(fasta_path,jobname)
+    def add_hash(x,y):
+        return x+"_"+hashlib.sha1(y.encode()).hexdigest()[:5]
+
+    basejobname = "".join(input_sequences)
+    basejobname = re.sub(r'\W+', '', basejobname)
+    target_id = add_hash(jobname, basejobname)
+
+    # Validate the input.
+    result_dir = Path(output_dir_base)
+    output_dir = os.path.join(output_dir_base, target_id)
+
+    sequences, is_multimer, _ = validate_input(
+        input_sequences=input_sequences,
+        symmetry_group='C1',
+        min_length=MIN_SINGLE_SEQUENCE_LENGTH,
+        max_length=MAX_SINGLE_SEQUENCE_LENGTH,
+        max_multimer_length=MAX_MULTIMER_LENGTH)
+
+    if is_multimer:
+        _, _, chain_mapping, _ = divide_multi_chains(target_id, output_dir, sequences, descriptions)
+    else:
+        chain_mapping = { 'A' : [] }
+
+    s = []
+    for des, seq in zip(descriptions, sequences):
+        s += [des, seq]
+
+    unique_sequences = []
+    [unique_sequences.append(x) for x in sequences if x not in unique_sequences]
+
+    if len(unique_sequences)==1:
+        homooligomers_num = len(sequences)
+    else:
+        homooligomers_num = 1
+
+    with open(f"{jobname}.fasta", "w") as f:
+        f.write("\n".join(s))
+    (
+        unpaired_msa,
+        paired_msa,
+        template_results,
+    ) = get_msa_and_templates(
+        target_id,
+        sequences,
+        result_dir=result_dir,
+        msa_mode="MMseqs2",
+        use_templates=use_templates,
+        homooligomers_num = homooligomers_num
+    )
+    _, homomers = chain_mapping.items()
+    for idx, seq in enumerate(sequences):
+        chain_id = PDB_CHAIN_IDS[idx]
+        head = homomers[idx][0] if len(sequences)>0 else chain_id
+        sequence_features = make_sequence_features(
+                sequence=seq, description=f'> {jobname} seq {chain_id}', num_res=len(seq)
+            )
+        monomer_msa = parsers.parse_a3m(unpaired_msa[idx])
+        msa_features = make_msa_features([monomer_msa])
+        template_features = template_results[idx]
+        feature_dict = {**sequence_features, **msa_features, **template_features}
+        feature_dict = compress_features(feature_dict)
+        os.makedirs(output_dir, exist_ok=True)
+        features_output_path = output_dir/f"{chain_id}.feature.pkl.gz"
+        features_h_path = output_dir/f"{head}.feature.pkl.gz"
+        shutil.copy(features_output_path, features_h_path)
+        
+        pickle.dump(
+            feature_dict,
+            gzip.GzipFile(features_output_path, "wb"),
+            protocol=4
+            )
+        if is_multimer:
+            multimer_msa = parsers.parse_a3m(paired_msa[idx])
+            pair_features = make_msa_features([multimer_msa])
+            pair_feature_dict = compress_features(pair_features)
+            uniprot_output_path = output_dir/f"{chain_id}.uniprot.pkl.gz"
+            uniprot_h_path = output_dir/f"{head}.uniprot.pkl.gz"
+            pickle.dump(
+                pair_feature_dict,
+                gzip.GzipFile(uniprot_output_path, "wb"),
+                protocol=4,
+            )
+            shutil.copy(uniprot_output_path, uniprot_h_path)
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Process a FASTA file with a max template date"
+    )
+    parser.add_argument(
+        "--fasta_path",
+        type=str,
+        required=True,
+        help="Path to the input FASTA file"
+    )
+
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        required=True,
+        help="Directory to write output files"
+    )
+
+    parser.add_argument(
+        "--max_template_date",
+        type=str,
+        required=True,
+        help="Maximum template date (YYYY-MM-DD)"
+    )
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    fasta_path = Path(args.fasta_path)
+    output_dir = Path(args.output_dir)
+    use_templates = str(args.max_template_date)>"1901"
+    make_mmseqs(fasta_path, output_dir, use_templates)
---
+++ scripts/prepare_precomputed.py
@@ -0,0 +1,135 @@
+from unifold.msa.templates import HhsearchHitFeaturizer
+from unifold.msa.pipeline import run_msa_tool, make_msa_features, make_sequence_features
+from unifold.msa import parsers
+from unifold.colab.data import read_a3m_file
+import pickle
+import os
+import gzip
+from pathlib import Path
+from unifold.msa.utils import get_chain_id_map
+from unifold.data.utils import compress_features
+import shutil
+import argparse
+
+MIN_SINGLE_SEQUENCE_LENGTH = 6
+MAX_SINGLE_SEQUENCE_LENGTH = 3000
+MAX_MULTIMER_LENGTH = 3000
+
+def create_dummy_file(file_path):
+    """Using touch method style (like Unix touch command)"""
+    dummy_path = file_path/"dummydummydummy.cif"
+    try:
+        # If file doesn't exist, create it
+        if not os.path.exists(dummy_path):
+            # Create directories first
+            os.makedirs(os.path.dirname(dummy_path), exist_ok=True)
+            # Create empty file
+            open(dummy_path, 'w').close()
+        # If file exists, just update its modification time
+        else:
+            os.utime(dummy_path, None)
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
+def get_empty_templates(query_sequence, parent_path):
+    create_dummy_file(parent_path)
+    pdb_template_hits = HhsearchHitFeaturizer(parent_path, "1900-01-01",0,None).get_templates(query_sequence, [])
+    return pdb_template_hits
+
+#like pipeline
+def prepare_msa(msa_path):
+    result = run_msa_tool(None, None, msa_path, "a3m", True)
+    parsed = parsers.parse_a3m(result["a3m"])
+    msa_features = make_msa_features((parsed))
+    return msa_features
+
+
+def write_separate_a3m(msa, msa_path:Path):
+    """
+    Write a string to a file at the specified path.
+    
+    Args:
+        msa (str): The string content to write to the file
+        msa_path (str): The file path where the string should be written
+    """
+    with open(msa_path, 'w', encoding='utf-8') as file:
+        file.write(msa)
+
+
+def process_precomputed(a3m_path:Path, parent_path:Path):
+    first_sequences, msas = read_a3m_file(a3m_path)
+    make_chains_txt(parent_path, first_sequences)
+    #msas = msas as description\nseq\n
+    chain_ids = "ABCDEFGHIJKLMNOPQRSTUVXYZ"[:len(first_sequences)]
+    descriptions = [f"seq_{it}" for it, _ in enumerate(first_sequences)]
+    _, _, mapping = get_chain_id_map(first_sequences, descriptions)
+    chain_ids, homomers = mapping.items()
+    for sequence, msa, chain_id, homomers in zip(first_sequences, msas, chain_ids, chain_ids, homomers):
+        msa_path = parent_path/f"{sequence}_single.a3m"
+        head = homomers[0] if len(homomers)>0 else chain_id
+        write_separate_a3m(msa, msa_path)
+        # how to concat msa_features?
+        msa_features = prepare_msa(msa_path)
+        sequence_features = make_sequence_features(sequence=sequence, description="first", num_res=len(sequence))
+        templates_results = get_empty_templates(sequence, parent_path)
+        feature_dict = {**sequence_features, **msa_features, **templates_results}
+        feature_dict = compress_features(feature_dict)
+        os.makedirs(parent_path, exist_ok=True)
+
+        features_output_path = parent_path/f"{chain_id}.feature.pkl.gz"
+        features_h_path = parent_path/f"{head}.feature.pkl.gz"
+
+        pickle.dump(
+            feature_dict,
+            gzip.GzipFile(features_output_path, "wb"),
+            protocol=4
+            )
+        shutil.copy(features_output_path, features_h_path)
+
+        if len(first_sequences)>1:
+            multimer_msa = parsers.parse_a3m(msa)
+            pair_features = make_msa_features([multimer_msa])
+            pair_feature_dict = compress_features(pair_features)
+            uniprot_output_path = parent_path/f"{chain_id}.uniprot.pkl.gz"
+            uniprot_h_path = parent_path/f"{head}.uniprot.pkl.gz"
+            pickle.dump(
+                pair_feature_dict,
+                gzip.GzipFile(uniprot_output_path, "wb"),
+                protocol=4,
+            )
+            shutil.copy(uniprot_output_path, uniprot_h_path)
+    #return {**sequence_features, **msa_features, **templates_results.features}
+
+
+def make_chains_txt(output_dir, input_sequences):
+    chain_order_path = os.path.join(output_dir, "chains.txt")
+    ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+    with open(chain_order_path, "w") as f:
+        f.write(" ".join([char for char in ALPHABET[:len(input_sequences)]] ))
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Process a precomputed a3m file"
+    )
+    parser.add_argument(
+        "--input_path",
+        type=str,
+        required=True,
+        help="Path to the input a3m file"
+    )
+
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        required=True,
+        help="Directory to write output files"
+    )
+    return parser.parse_args()  
+
+if __name__ == "__main__":
+    args = parse_args()
+    fasta_path = Path(args.fasta_path)
+    output_dir = Path(args.output_dir)
+    process_precomputed(fasta_path, output_dir)
